c002c13c05fff3aace76d387f95edd38e9a003bf
Matthew Nelson <matthewpeternelson@gmail.com>
2022-11-14 20:30:26 -0700

    Functional Generator ready to go

---
+++ libs/sheets.js
@@ -0,0 +1,30 @@
+import { google } from "googleapis";
+export async function getNamesData() {
+  try {
+    const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+    const jwt = new google.auth.JWT(
+      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+      null,
+      (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+      target
+    );
+
+    const sheets = google.sheets({ version: "v4", auth: jwt });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
+      range: "names", // sheet name
+    });
+
+    const rows = response.data.values;
+    if (rows.length) {
+      return rows.map((row) => ({
+        name: row[0] ? row[0] : null,
+        oneAgo: row[1] ? row[1] : null,
+        twoAgo: row[2] ? row[2] : null,
+      }));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return [];
+}
